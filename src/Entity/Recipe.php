6d0aff7a7ce240005844f6f89a8701059e5637d6
armelo483 <armelo483@users.noreply.github.com>
2022-07-06 19:59:27 +0200

    Ajout multi image pour une recette

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -7,9 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=RecipeRepository::class)
+ * @Vich\Uploadable
  */
 class Recipe
 {
@@ -37,7 +41,7 @@ class Recipe
     private $created_at;
 
     /**
-     * @ORM\Column(type="datetime_immutable")
+     * @ORM\Column(type="datetime_immutable", nullable=true)
      */
     private $updated_at;
 
@@ -45,6 +49,12 @@ class Recipe
      * @ORM\Column(type="string", length=255)
      */
     private $image_url;
+
+    /**
+     * @Vich\UploadableField(mapping="recipe_images", fileNameProperty="image_url")
+     * @var File
+     */
+    private $image_file;
 
     /**
      * @ORM\Column(type="float")
@@ -76,10 +86,18 @@ class Recipe
      */
     private $niveau_difficulte;
 
+    /**
+     * @ORM\OneToMany(targetEntity=RecipePhoto::class, mappedBy="recipe", cascade={"persist"})
+     */
+    private $recipePhotos;
+
     public function __construct()
     {
         $this->categories = new ArrayCollection();
         $this->ingredients = new ArrayCollection();
+        $this->created_at = new \DateTimeImmutable();
+        $this->budget = 3000; $this->image_url = '';
+        $this->recipePhotos = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -253,5 +271,66 @@ class Recipe
 
         return $this;
     }
+
+    public function isIsMisEnAvant(): ?bool
+    {
+        return $this->isMisEnAvant;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile(): ?File
+    {
+        return $this->image_file;
+    }
+
+    /**
+     * @param File|null $image_file
+     * @return Recipe
+     */
+    public function setImageFile(?File $image_file = null): Recipe
+    {
+        $this->image_file = $image_file;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image_file) {
+            $this->updated_at = new \DateTimeImmutable('now');
+        }
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, RecipePhoto>
+     */
+    public function getRecipePhotos(): Collection
+    {
+        return $this->recipePhotos;
+    }
+
+    public function addRecipePhoto(RecipePhoto $recipePhoto): self
+    {
+        if (!$this->recipePhotos->contains($recipePhoto)) {
+            $this->recipePhotos[] = $recipePhoto;
+            $recipePhoto->setRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecipePhoto(RecipePhoto $recipePhoto): self
+    {
+        if ($this->recipePhotos->removeElement($recipePhoto)) {
+            // set the owning side to null (unless already changed)
+            if ($recipePhoto->getRecipe() === $this) {
+                $recipePhoto->setRecipe(null);
+            }
+        }
+
+        return $this;
+    }
+
 
 }
